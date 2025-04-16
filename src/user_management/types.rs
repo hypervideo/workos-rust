9de7cafcb3e2be8796e2b46590ada4ee37380444
Daniëlle Huisman <danielle@hyper.video>
2025-04-16 15:14:52 +0200

    feat(user-management): add get logout URL (#12)

--- src/user_management/types.rs
+++ src/user_management/types.rs
@@ -1,9 +1,11 @@
 mod client_secret;
 mod impersonator;
 mod refresh_token;
+mod session_id;
 mod user;
 
 pub use client_secret::*;
 pub use impersonator::*;
 pub use refresh_token::*;
+pub use session_id::*;
 pub use user::*;
